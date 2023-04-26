a249017685e0439d2cb5e89a2b7eb7dbfd69294e
Jeffrey A <agyepong99@gmail.com>
2023-04-26 06:20:31 -0600

    Initial commit

---
+++ resources/js/Components/Frontend/About.vue
@@ -0,0 +1,54 @@
+<template>
+  <section id="about" class="section bg-light-secondary dark:bg-dark-secondary">
+    <div
+      class="container mx-auto"
+      v-motion
+      :initial="{
+        opacity: 0,
+        y: 100,
+      }"
+      :visible="{
+        opacity: 1,
+        y: 0,
+      }"
+    >
+      <div class="flex flex-col xl:flex-row gap-24">
+              <img
+              class="object-cover h-[50%] w-[50%] md:mx-auto lg:mx-0 rounded-2xl"
+              src="img/about.jpg"
+              alt="about"
+              />
+        <div
+          class="
+            flex flex-col
+            items-center
+            text-center
+            lg:items-start lg:text-left
+          "
+        >
+          <div class="flex flex-col">
+            <h2 class="text-3xl lg:text-4xl font-medium lg:font-extrabold mb-3">
+              About Me
+            </h2>
+            <P class="mb-4 text-accent">Freelance frontend Web developer</P>
+            <hr class="mb-8 opacity-90 dark:opacity-5" />
+            <p class="mb-8">
+              As a self-taught freelancer turned NAIT student, I am a passionate web developer
+               and designer with a keen eye for quality and customer satisfaction. My experience includes developing modern, 
+              user-friendly websites for small businesses, e-commerce stores, and personal portfolios
+               <br />
+              <br />
+              I specialize in creating fully responsive websites optimized for performance and search engine rankings,
+               using the latest technologies such as HTML, CSS, JavaScript, Vue.js, PHP, and CMS platforms like WordPress. 
+               With a focus on timely delivery and customer satisfaction, 
+              I am dedicated to providing high-quality work that exceeds client expectations. 
+            </p>
+          </div>
+          <a href="#contact" class="btn btn-md bg-accent text-light-tail-100">
+            Contact me
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
---
+++ resources/js/Components/Frontend/Skills.vue
@@ -0,0 +1,33 @@
+<script setup>
+defineProps({
+  skills: Object,
+});
+</script>
+<template>
+  <!-- <section class="bg-light-tail-100 dark:bg-dark-navy-500 py-16">
+    <div class="container mx-auto">
+      <h2 class="section-title text-center">Skills</h2>
+      <div class="grid grid-cols-8 md:grid-flow-col">
+        <div
+          class="flex items-center justify-center"
+          v-for="skill in skills.data"
+          :key="skill.id"
+        >
+          <img :src="skill.image" :alt="skill.name" class="lg:h-20" />
+        </div>
+      </div>
+    </div>
+  </section> -->
+  <section class="bg-light-tail-100 dark:bg-dark-navy-500 py-16">
+  <div class="container mx-auto">
+    <h2 class="section-title text-center">Skills</h2>
+    <div class="flex flex-wrap md:flex-nowrap lg:flex-nowrap justify-center -mx-4">
+      <div class="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 px-4 my-4" v-for="skill in skills.data" :key="skill.id">
+        <img :src="skill.image" :alt="skill.name" class="h-auto mx-auto max-w-full" style="max-width: 150px;" />
+      </div>
+    </div>
+  </div>
+</section>
+
+
+</template>
